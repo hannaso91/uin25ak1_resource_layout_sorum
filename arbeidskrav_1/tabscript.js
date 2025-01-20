1ec790bea659502ad5fb2021097b5442d46ef208
hannaso91 <hannaso@hiof.no>
2025-01-20 21:56:18 +0100

    fått til dynamisk forandring

--- arbeidskrav_1/tabscript.js
+++ arbeidskrav_1/tabscript.js
@@ -2,7 +2,7 @@ let resourceHTML = ""
 
 resources.map(resource => resourceHTML += `
     <li>
-        <p class="tab" onclick="updateContent">${resource.category}</p>
+        <p class="tab" onclick="updateContent('${resource.category}')">${resource.category}</p>
     </li>
 `) 
 
@@ -13,16 +13,20 @@ document.getElementById("navtab").innerHTML = resourceHTML
 function updateContent(categoryName) {
     const filtered = resources.filter(resource => resource.category === categoryName);
     const selectedCategory = filtered[0]
-
-    console.log(filtered)
-
     const contentHTML = `
         <article>
             <h2 id="headtext">${selectedCategory.category}</h2>
             <p id="text">${selectedCategory.text}</p>
+            <ul>
+                <li>
+                    
+                </li>
+            </ul>
         </article>`
 
-    document.getElementById("whiteContainer").innerHTML = contentHTML
+    document.getElementById("whiteContainer").innerHTML = contentHTML 
+    
+   
 }
 
 updateContent("CSS")
